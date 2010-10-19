421d1b6658c8a276c23f510fb0426a4bb86837e2
Steven Heidel <steven@livingskyweb.ca>
2010-10-18 23:04:46 -0600

    Remove most of the default stuff that came with a generated application.

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,20 @@ class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
     # Super must be called *after* settings @tabs!
-    @tabs = nil
+    @tabs = [
+      { :label => "Countdown", :action => '/app', :icon => "/public/images/tabs/11-clock.png", :reload => true }, 
+      { :label => "News",  :action => '/app/News',  :icon => "/public/images/tabs/18-envelope.png" },
+      { :label => "Sponsors",  :action => '/app/Sponsors',  :icon => "/public/images/tabs/112-group.png" },
+      { :label => "More",   :action => '/app/More', :icon => "/public/images/tabs/09-chat2.png" }
+    ]
     @@tabbar = nil
 
     super
+    
+    @default_menu = {
+      "Home" => :home,
+      "Refresh" => :refresh
+    }
 
     # Uncomment to set sync notification callback to /app/Settings/sync_notify.
     # SyncEngine::set_objectnotify_url("/app/Settings/sync_notify")
